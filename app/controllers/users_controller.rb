ac20b269118f7c437f2faf3280eb615065847282
David Marshall <dzmarshall@gmail.com>
2014-08-05 13:19:42 -0400

    Add new user mailer create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,32 @@
 class UsersController < ApplicationController
+  before_action :generate_password, only: :create
   def new
     @user = user_type.new
   end
 
   def create
     @user = user_type.new(user_params)
+    @user.password = @password
+    @user.password_confirmation = @password
     @user.save
+    @user.send_reset_password_instructions
   end
 
   private
 
   def user_params
-    params.require(require_user).permit(:email, :password, :password_confirmation, :type)
+    params.require(required_user).permit(:email, :password, :password_confirmation, :type)
   end
 
   def user_type
     params[:type].constantize
   end
 
-  def require_user
+  def required_user
     params[:type].downcase.to_sym
+  end
+
+  def generate_password
+    @password = Devise.friendly_token[0,20]
   end
 end
