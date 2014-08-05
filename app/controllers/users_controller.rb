dadb6fef73237d176d05d29558d8a2288a67aaab
David Marshall <dzmarshall@gmail.com>
2014-08-05 12:10:13 -0400

    Add users controller, works with admin and volunteers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def new
+    @user = user_type.new
+  end
+
+  def create
+    @user = user_type.new(user_params)
+    @user.save
+  end
+
+  private
+
+  def user_params
+    params.require(require_user).permit(:email, :password, :password_confirmation, :type)
+  end
+
+  def user_type
+    params[:type].constantize
+  end
+
+  def require_user
+    params[:type].downcase.to_sym
+  end
 end
