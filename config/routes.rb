7333aad312b937ab0c73130c0562d5148bcb52b8
David Marshall <dzmarshall@gmail.com>
2014-08-05 23:08:16 -0400

    Add simple user_controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users, only: [:new, :create]
   resources :admins, controller: 'users', only: [:new, :create], type: 'Admin'
   resources :volunteers, controller: 'users', only: [:new, :create], type: 'Volunteer'
 end
