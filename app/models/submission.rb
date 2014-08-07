0bc233944c3600ab08a1f4f7e57226312b82b094
jparker85 <jparker85@gmail.com>
2014-08-07 12:24:58 -0400

    adding devise views

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,9 +2,9 @@ class Submission < ActiveRecord::Base
   acts_as_taggable
   has_many :attachments
   has_many :submissions, :through => :application_processes
-  has_many :fests, :through => :application_processes
+  # has_many :fests, :through => :application_processes
 
   accepts_nested_attributes_for :attachments, :reject_if => :all_blank, :allow_destroy => true
-  accepts_nested_attributes_for :participant
+  # accepts_nested_attributes_for :participant
 
 end
