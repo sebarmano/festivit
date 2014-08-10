689bb9960da191e661f3b198c2846f75e8ccbd5b
maebeale <maebeale@gmail.com>
2014-08-10 19:07:45 -0400

    WIP: ticket importer contd

--- app/models/ticket_importer.rb
+++ app/models/ticket_importer.rb
@@ -14,11 +14,12 @@ class TicketImporter < ActiveImporter::Base
   column 'Date', :date_time
   column 'Order Status', :status
   column 'Item Amount', :qty
-  column 'Item Name', :name
-
-
+  column 'Item Name', :item_name
   column 'Item SKU', :ticket_type_id do |sku|
     Ticket_type.find_by(name: sku)
+  end
+  column 'Billing Last Name', :participant_id do |lname, fname|
+    Participant.find_by(name: lname, fname)
   end
 
   on :import_failed do |ex|
@@ -40,25 +41,25 @@ class TicketImporter < ActiveImporter::Base
 
   #TRYING TO GET PID FOR CURRENT PARTICIPANT------------
 
-  column 'Billing Last Name', :participant_id do |sku|
-    Participant.find_by(name: sku)
-  end
-
-  on :row_processing do
-    if row['Billing Last Name'].present?
-      tags = row['Tags'].split(',').map(&:strip).map(&:downcase)
-      model.participant_id = !!tags.delete('manager')
-      model.tag_ids = tags.map { |tag| Tag.where(name: tag).first_or_create }
-    end
-  end
-
-  def find_participant do
-    fetch_model do
-      Participant.where(
-          fname: row['First name'],
-          lname: row['Last name'],
-          email: row['Billing Email'],
-      ).first_or_initialize.id #pull participant ID
-    end
-  end
+  # column 'Billing Last Name', :participant_id do |sku|
+  #   Participant.find_by(name: sku)
+  # end
+  #
+  # on :row_processing do
+  #   if row['Billing Last Name'].present?
+  #     tags = row['Tags'].split(',').map(&:strip).map(&:downcase)
+  #     model.participant_id = !!tags.delete('manager')
+  #     model.tag_ids = tags.map { |tag| Tag.where(name: tag).first_or_create }
+  #   end
+  # end
+  #
+  # def find_participant do
+  #   fetch_model do
+  #     Participant.where(
+  #         fname: row['First name'],
+  #         lname: row['Last name'],
+  #         email: row['Billing Email'],
+  #     ).first_or_initialize.id #pull participant ID
+  #   end
+  # end
 end
