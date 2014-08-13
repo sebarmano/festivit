2a3f43119b60f7d42733f727b6a3ae7c71ba8abe
David Marshall <dzmarshall@gmail.com>
2014-08-13 10:09:59 -0400

    Add some specs

--- spec/features/participant_feature_spec.rb
+++ spec/features/participant_feature_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 feature "Participant" do
-  scenario "Band applies to festival" do
+  def apply
     visit "/"
     click_link "Apply"
+    expect(page).to have_text("Select what role you would like to apply for:")
+    @participant = create(:participant)
+  end
+
+  def fill_in_details
+    fill_in 'First Name', :with => @participant.fname
+    fill_in 'Last Name', :with => @participant.lname
+    fill_in 'Email', :with => 'fakeemail@myfakeemail.com'
+    fill_in 'Password', :with => 'password'
+    fill_in 'Password confirmation', :with => 'password'
+    click_button 'Create Participant'
+  end
+
+  scenario "craft vendor applies to festival" do
+    apply
+    fill_in_details
   end
 end
