428cadbd9d44a0587dc25527ffc6704dc581a517
Katie Southers <katiesouthers@Katies-MacBook-Pro.local>
2014-08-14 14:43:50 -0400

    updated attachments

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -5,6 +5,7 @@ class SubmissionsController < ApplicationController
 
   def index
     @submissions = Submission.all
+
   end
 
   def show
@@ -18,6 +19,7 @@ class SubmissionsController < ApplicationController
     @participant = Participant.find(params[:participant_id])
     @submission = @participant.submissions.new
     @role = @participant.role_types.first.name
+    @types = ['song', 'video', 'photo']
     authorize_action_for(@submission)
   end
 
