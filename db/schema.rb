1fee79b60147404bf9d327a3057bff6120f50222
jparker85 <jparker85@gmail.com>
2014-08-16 18:34:09 -0400

    Bringing in real data

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20140813191907) do
-=======
 ActiveRecord::Schema.define(version: 20140814144722) do
->>>>>>> upstream/release-1
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
