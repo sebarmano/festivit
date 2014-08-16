c75c7b37846666c7e455ba11b9add31cd09c8cf9
jparker85 <jparker85@gmail.com>
2014-08-16 18:32:30 -0400

    Bringing in real data

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require carousel
 //= require cocoon
 //= require_tree .
 
--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -5,7 +5,6 @@ class SubmissionsController < ApplicationController
 
   def index
     @submissions = Submission.all
-
   end
 
   def show
