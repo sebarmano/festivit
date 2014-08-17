3b3f4f378a946d30639c50ea429d59937b7de735
Sebastian Armano <sebarmano@gmail.com>
2014-08-16 22:45:11 -0400

    Added specs for participant model

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -4,4 +4,9 @@ describe Participant do
   it {is_expected.to validate_presence_of(:lname)}
   it {is_expected.to validate_presence_of(:fname)}
   it {is_expected.to validate_uniqueness_of(:lname).scoped_to(:fname, :email)}
+
+  it "returns a contact's full name as a string" do
+    contact = FactoryGirl.create(:participant, fname: "John", lname: "Smith")
+    expect(contact.name).to eq("Smith, John")
+  end
 end
