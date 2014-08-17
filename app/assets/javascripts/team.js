6726ffaafb2810126eb13ab9ebbb19e846ba7c3d
Talal Obeid <talal.obeid@gmail.com>
2014-08-17 13:16:57 -0400

    better and leaner javascript for randomization

--- app/assets/javascripts/team.js
+++ app/assets/javascripts/team.js
@@ -1,21 +1,6 @@
 $(document).ready(function(){
-      $('.team').each(function(){
-            // get current ul
-            var $ul = $(this);
-            // get array of list items in current ul
-            var $liArr = $ul.children('li');
-            // sort array of list items in current ul randomly
-            $liArr.sort(function(a,b){
-                  // Get a random number between 0 and 10
-                  var temp = parseInt( Math.random()*10 );
-                  // Get 1 or 0, whether temp is odd or even
-                  var isOddOrEven = temp%2;
-                  // Get +1 or -1, whether temp greater or smaller than 5
-                  var isPosOrNeg = temp>5 ? 1 : -1;
-                  // Return -1, 0, or +1
-                  return( isOddOrEven*isPosOrNeg );
-            })
-            // append list items to ul
-            .appendTo($ul);
-      });
+      var ul = document.querySelector('.team');
+        for (var i = ul.children.length; i >= 0; i--) {
+          ul.appendChild(ul.children[Math.random() * i | 0]);
+}
 });
