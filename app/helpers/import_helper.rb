d46c3adf36480ac9a7f4c3dcc3709ee84713fbcd
maebeale <maebeale@gmail.com>
2014-08-21 14:55:01 -0400

    rename helper method to make it unique

--- app/helpers/import_helper.rb
+++ app/helpers/import_helper.rb
@@ -1,8 +1,9 @@
 module ImportHelper
-  def demoday
-    importer = ImporterDemoday.new('1KFOCGLPaSsE-ey1IzQiw03YEmryZuutwMtT-1KxRQJQ',
+  def demoday_importer
+      importer = ImporterDemoday.new('1KFOCGLPaSsE-ey1IzQiw03YEmryZuutwMtT-1KxRQJQ',
                                    :user => 'maedevelops@gmail.com',
                                    :password => 'mypass1234')
+                                   #TODO - set up env variables in Heroku to replace this fake acct
     importer.import
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -51,6 +51,9 @@ Rails.application.routes.draw do
   get 'participants/import_demoday', to: "participants#import_demoday", :as => :import_demoday
   post 'participants/import_demoday', to: "participants#import_demoday"
 
+  get 'participants/import_demoday_google', to: "participants#import_demoday_google", :as => :import_demoday_google
+  post 'participants/import_demoday_google', to: "participants#import_demoday_google"
+
   get 'role_types/import', to: "role_types#import", :as => :import_role_types
   post 'role_types/import', to: "role_types#import"
 
