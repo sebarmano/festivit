92d9c789eccfbf25eacc5eb5edcd7d2c7a8f9246
maebeale <maebeale@gmail.com>
2014-08-25 15:26:34 -0400

    change demoday button to ncstars

--- app/helpers/import_helper.rb
+++ app/helpers/import_helper.rb
@@ -1,6 +1,6 @@
 module ImportHelper
   def demoday_importer
-      importer = ImporterDemoday.new(ENV["GOOGLE_SHEET_DEMODAY"],
+    importer = ImporterDemoday.new(ENV["GOOGLE_SHEET_NCSTARS"],
                                    :user => ENV["GOOGLE_ACCT"],
                                    :password => ENV["GOOGLE_PWD"])
     importer.import
